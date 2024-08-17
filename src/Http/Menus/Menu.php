558226ab86c7f4aa290c91096ac71e72692b2d67
Narsilien <rigaux.jonathan@gmail.com>
2024-08-17 11:00:00 +0200

    Add menu

---
+++ src/Http/Menus/Menu.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Narsil\Localization\Http\Menus;
+
+#region USE
+
+use Narsil\Menus\Enums\VisibilityEnum;
+use Narsil\Menus\Http\Menus\AbstractMenu;
+use Narsil\Menus\Models\MenuNode;
+
+#endregion
+
+/**
+ * @version 1.0.0
+ *
+ * @author Jonathan Rigaux
+ */
+class Menu extends AbstractMenu
+{
+    #region PUBLIC METHODS
+
+    /**
+     * @return array
+     */
+    public static function getBackendMenu(): array
+    {
+        return [
+            [
+                MenuNode::LABEL => 'Languages',
+                MenuNode::URL => '/backend/languages',
+                MenuNode::RELATIONSHIP_ICON => 'lucide/languages',
+            ],
+            [
+                MenuNode::LABEL => 'Translations',
+                MenuNode::URL => '/backend/translations',
+                MenuNode::RELATIONSHIP_ICON => 'lucide/book-a',
+            ],
+        ];
+    }
+
+    #endregion
+}
