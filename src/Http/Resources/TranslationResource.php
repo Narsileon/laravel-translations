49108e20d94c1d233c72fe6dc37d5a22b6255843
Narsilien <rigaux.jonathan@gmail.com>
2024-09-01 13:05:50 +0200

    Add resources

---
+++ src/Http/Resources/TranslationResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Narsil\Localization\Http\Resources;
+
+#region USE
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Narsil\Localization\Models\Translation;
+
+#endregion
+
+/**
+ * @version 1.0.0
+ *
+ * @author Jonathan Rigaux
+ */
+class TranslationResource extends JsonResource
+{
+    #region PUBLIC METHODS
+
+    /**
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            Translation::DEFAULT_VALUE => $this->{Translation::DEFAULT_VALUE},
+            Translation::ID => $this->{Translation::ID},
+
+            Translation::RELATIONSHIP_VALUES => $this->{Translation::RELATIONSHIP_VALUES}
+        ];
+    }
+
+    #endregion
+}
---
+++ src/Http/Resources/TranslationValueResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Narsil\Localization\Http\Resources;
+
+#region USE
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Narsil\Localization\Models\Language;
+use Narsil\Localization\Models\TranslationValue;
+
+#endregion
+
+/**
+ * @version 1.0.0
+ *
+ * @author Jonathan Rigaux
+ */
+class TranslationValueResource extends JsonResource
+{
+    #region PUBLIC METHODS
+
+    /**
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            TranslationValue::VALUE => $this->{TranslationValue::VALUE},
+
+            Language::LOCALE => $this->{TranslationValue::RELATIONSHIP_LANGUAGE}->{Language::LOCALE},
+        ];
+    }
+
+    #endregion
+}
